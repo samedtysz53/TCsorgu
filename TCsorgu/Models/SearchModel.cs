c7478d533fa48bc73206a0a6ae66198d5e14e15a
samed_tysz <samed_tysz@hotmail.com>
2023-04-16 02:33:12 +0300

    Küçük eklemeler yapıldı

--- TCsorgu/Models/SearchModel.cs
+++ TCsorgu/Models/SearchModel.cs
@@ -1,11 +1,17 @@
-﻿namespace TCsorgu.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TCsorgu.Models
 {
     public class SearchModel
     {
+        [Required(ErrorMessage ="Lütfen Tc kimlik numaranızı giriniz")]
         public string TC { get; set; }
+        [Required(ErrorMessage = "Lütfen adınızı giriniz")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Lütfen soyadınızı giriniz")]
         public string surname { get; set; }
-        public int year { get; set; }
+        [Required(ErrorMessage = "Lütfen doğum yılınızı giriniz")]
+        public string year { get; set; }
         public bool İsChecked { get; set; }
 
     }
